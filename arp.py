51b5e9b521fad55b19ec740d8443e611f93733e7
kaine119 <9492636+kaine119@users.noreply.github.com>
2023-03-28 07:29:15 +0800

    Change subnet for second router

--- arp.py
+++ arp.py
@@ -56,7 +56,7 @@ def find_ips_by_number(test_numbers: List[int]) -> dict[str, str]:
 
 
 def arp_scan():
-    target_ip = "192.168.50.1/24"
+    target_ip = "192.168.0.1/24"
     arp = ARP(pdst=target_ip)
     ether = Ether(dst="ff:ff:ff:ff:ff:ff")
     packet = ether/arp
