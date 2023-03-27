ab7039634721566429f253d2781d3812d6c57807
ashley-koh <ashleykoh24@gmail.com>
2023-03-28 02:44:01 +0800

    added different parties as indiv behaviours

--- find_victims.py
+++ find_victims.py
@@ -8,11 +8,12 @@ my_tellos = get_mac_addr_from_num([10])
 print(my_tellos)
 
 def bonus_search_party(tellos = []):
+    S1, S2, S3 = tellos
     with fly.sync_these():
         fly.straight_from_pad(400, 0, 80, 50, 'm3', tello=tellos[0])
-        fly.straight_from_pad(400, 0, 80, 50, 'm3', tello=tellos[1])
-    with fly.sync_these():
         fly.reorient(height=80, pad='m7', tello=tellos[1])
+    with fly.sync_these():
+        fly.straight_from_pad(400, 0, 80, 50, 'm3', tello=tellos[1])
         fly.reorient(height=80, pad='m7', tello=tellos[0])
     with fly.sync_these():
         fly.land(tello=tellos[0])
@@ -21,8 +22,6 @@ def bonus_search_party(tellos = []):
         fly.flight_complete(tello=tellos[0])
         fly.flight_complete(tello=tellos[1])
 
-    
-
 def start_search_party(tellos = []):
     with fly.sync_these():
         fly.right(dist=100, tello=tellos[0])
@@ -30,6 +29,18 @@ def start_search_party(tellos = []):
     with fly.sync_these():
         fly.flight_complete(tello=tellos[0])
         fly.flight_complete(tello=tellos[1])
+
+def bottom_search_party(tellos = []):
+    with fly.sync_these():
+
+def top_search_party(tellos = []):
+    with fly.sync_these():
+
+def outside_search_party(tellos = []):
+    with fly.sync_these():
+
+def landing_party(tellos = []):
+
     
 
 with FlyTello(my_tellos) as fly:
@@ -37,16 +48,21 @@ with FlyTello(my_tellos) as fly:
     fly.set_pad_detection(direction='downward')
     fly.takeoff()
 
-    # with fly.individual_behaviours():
-    #     fly.run_individual(bonus_search_party, tellos=get_mac_addr_from_num([6, 10]))
-    #     fly.run_individual(start_search_party, tellos=get_mac_addr_from_num([11, 18]))
+    with fly.individual_behaviours():
+        fly.run_individual(bonus_search_party, tellos=get_mac_addr_from_num([6, 10]))
+        fly.run_individual(start_search_party, tellos=get_mac_addr_from_num([11, 18]))
+        fly.run_individual(bottom_search_party, tellos=get_mac_addr_from_num([11, 18]))
+        fly.run_individual(top_search_party, tellos=get_mac_addr_from_num([11, 18]))
+        fly.run_individual(outside_search_party, tellos=get_mac_addr_from_num([11, 18]))
+        
+        
 
     # Window algo
-    fly.straight_from_pad(200, 0, 80, 50, 'm7')
-    fly.reorient(height=80, pad='m7')
-    fly.rotate_cw(angle=90)
-    fly.straight_from_pad(150, 0, 80, 80, 'm7')
-    fly.reorient(height=80, pad='m2')
+    # fly.straight_from_pad(200, 0, 80, 50, 'm7')
+    # fly.reorient(height=80, pad='m7')
+    # fly.rotate_cw(angle=90)
+    # fly.straight_from_pad(150, 0, 80, 80, 'm7')
+    # fly.reorient(height=80, pad='m2')
     
 
     fly.land()
