0fdb51c6094bb382f7e601b2944439d752c3e4f3
pyraxo <pyraxo@pyraxo.moe>
2023-03-28 03:10:02 +0800

    refactor window pass to fit functions format

--- individual_routines/window.py
+++ individual_routines/window.py
@@ -1,6 +1,7 @@
-def pass_window(tello, pad, fly):
-    fly.straight_from_pad(200, 0, 80, 50, pad)
-    fly.reorient(height=80, pad=pad)
-    fly.rotate_cw(angle=90)
-    fly.straight_from_pad(150, 0, 80, 80, pad)
-    fly.reorient(height=80, pad=pad)
+def pass_window(fly, tellos=[], pad="m7"):
+    with fly.sync_these():
+        fly.straight_from_pad(200, 0, 80, 50, pad)
+        fly.reorient(height=80, pad=pad)
+        fly.rotate_cw(angle=90)
+        fly.straight_from_pad(150, 0, 80, 80, pad)
+        fly.reorient(height=80, pad=pad)
